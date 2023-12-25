972b616e119c75fabdec135026c445c23b8ee4f3
MTcom2 <MTlibralyCon2@gmail.com>
2023-12-25 16:00:14 +0700

    Add new file

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        int select;
+        Scanner sc=new Scanner();
+         System.out.print("Select choice : ");
+        select=sc.nextInt();
+       switch (select) {
+        case 1:
+            
+            break;
+       
+       case 2:
+
+            break;
+       }
+
+    }
+}
